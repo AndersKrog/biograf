1a8678b109dc314051adbf2d70862179904007ba
Borgerskabet <70210262+Borgerskabet@users.noreply.github.com>
2020-09-10 14:31:28 +0200

    Add files via upload

---
+++ Database/Database/Databaselag/Sql.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace H1CaseSQLTableOrDataReader.Databaselag
+{
+    /// <summary>
+    /// ConnectionString indeholder navn på SQL Server, Databasenavn samt sikkerhedsoplysninger, f.eks. hvordan logges på m.m.
+    /// Her er der refereret til lokalhost i stedet for til en navngiven server. 
+    /// private static string ConnectionString = "Data Source=TEC-5350-LA0116;Initial Catalog=sqleksempler; Integrated...
+    /// </summary>
+    static class SQL
+    {
+        private static string ConnectionString = "Data Source=localhost;Initial Catalog=biograf; Integrated Security=SSPI;Connect Timeout=5;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static bool SqlConnectionOK()
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+        }
+
+        // Create, Read, Update og Delete (CRUD)
+
+        //1) Create, Data der skal creates i en tabel (det hedder insert på sql'sk)
+        public static void insert(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void Update(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        //2) Read, Data der skal læses fra en tabel (det hedder select på sql'sk)
+
+        //2a) DataAdapter og DataTable, returnere DataTable
+        public static DataTable ReadTable(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                DataTable records = new DataTable();
+
+                //Create new DataAdapter
+                using (SqlDataAdapter a = new SqlDataAdapter(sql, con))
+                {
+                    //Use DataAdapter to fill DataTable records
+                    con.Open();
+                    a.Fill(records);
+                }
+
+                return records;
+            }
+        }
+
+        //2b) Read med Datareader, udskriver med Console.WriteLine()
+        public static void DataReader()
+        {
+            Console.WriteLine("DataReader");
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Kunder", con);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                //Er der rækker?
+                Console.WriteLine(reader.HasRows);
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string navn = reader.GetString(1);
+                    string adr = reader.GetString(3);
+                    int alder = reader.GetInt32(4);
+
+                    Console.WriteLine($"Id: {id} navn: {navn} adresse: {adr} - alder: {alder}");
+                }
+
+            }
+        }
+        public static void DeleteData(string sqldel)
+        {
+            // der skal være en form for tjek, om det ønskede opslag findes
+
+            Console.WriteLine("DeleteData");
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sqldel, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Database/Database/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using H1CaseSQLTableOrDataReader.Databaselag;
+using H1CaseSQLTableOrDataReader.Model;
+
+namespace H1CaseSQLTableOrDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (SQL.SqlConnectionOK())
+            {
+                Console.WriteLine("Der er forbindelse til databasen");
+            }
+            else
+            {
+                Console.WriteLine("Der er ikke forbindelse til databasen");
+            }
+
+            //Opretter ny kunde i main og kalder metode, så kunden bliver oprettes i databasen
+
+            // Kunde k1 = new Kunde("TEST", "Zperson", "normal", "Nattestien 47", 30, 25555564);
+            // k1.InsertIntoDB();
+
+            // opdaterer en kunde med angivet id i angivet felt med angivet værdi:
+            // Kunde.UpdateInDB(1, "fornavn", "Kent");
+
+
+            //Læser alle kunder i tabellen Kunder og danner liste vha. datatabel
+            List<Kunde> alleKunder = Kunde.DanKundeListe();
+
+            //som udskrives her
+            foreach (var item in alleKunder)
+            {
+                Console.WriteLine(item.Fornavn + " " + item.Adresse + " Alder: " + item.Alder + " ID: " + item.Kundeid);
+            }
+
+            // sorterer listen efter kundernes efternavn
+            // alleKunder.Sort();
+
+            // sletter en kunde og kundens ordre i databasen
+            //Kunde.DeleteInDB(50);
+
+            //opretter liste over en kundes ordre, parameter er kundens ordrenummer
+            
+            List<Ordre> ordreliste = Kunde.DanOrdreListe(1);
+            foreach (var item in ordreliste)
+            {
+                Console.WriteLine(item.Ordreid + " " + item.Kundeid + " Spilletidspunkt: " + item.SpilleTidspunkt);
+            }
+            
+            //SQL.DataReader();
+
+            Console.ReadKey();
+        }
+    }
+}
